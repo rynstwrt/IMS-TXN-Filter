3fee6f7145bba809017cc3caf81d05a98c25511c
Ryan Stewart <ryanstewartalex@gmail.com>
2023-04-21 18:55:01 -0500

    Begin HTML

--- js/content.js
+++ js/content.js
@@ -4,9 +4,26 @@ for (let i = 0; i < tableRows.length; ++i)
 {
     const row = tableRows[i];
 
-    for (let j = 0; j < row.children.length; ++j)
-    {
-        console.log(row.children[j]);
-    }
-    console.log("------")
+    const transactionTypeTD = row.children[5];
+    if (!transactionTypeTD) continue;
+
+    const type = transactionTypeTD.textContent.trim();
+
+    // if (type === "Cloud Instance Reclaim")
 }
+
+
+/*
+Cloud Instance Reclaim
+Cloud Instance Reload
+Cloud Migrate
+Cloud Provision
+Create Cloud Disk Checksum
+Export Image COS
+Import Image COS
+MS Cloud Provision
+RH Reload
+Reclaim
+UBU Reload
+XenServer HV Reload
+ */
