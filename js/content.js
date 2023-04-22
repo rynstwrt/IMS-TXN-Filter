566d331e778467509c769412c88507949a42617a
Ryan Stewart <ryanstewartalex@gmail.com>
2023-04-22 15:58:21 -0500

    Update Style and Clean Up

--- js/content.js
+++ js/content.js
@@ -1,41 +1,3 @@
-const validTXNTypes = ["Cloud Instance Reclaim", "Cloud Instance Reload", "Cloud Migrate", "Cloud Provision", "Create Cloud Disk Checksum",
-                        "Export Image COS", "Import Image COS", "MS Cloud Provision", "RH Reload", "Reclaim",
-                        "Storage Provision", "UBU Reload", "XenServer HV Reload"];
-
-browser.runtime.onMessage.addListener(message =>
-{
-    const selected = message.selected;
-    console.log(selected);
-
-    const tableRows = document.querySelectorAll("#layoutContentBody tbody tr");
-
-    for (let i = 0; i < tableRows.length; ++i) {
-        const row = tableRows[i];
-
-        const transactionTypeTD = row.children[5];
-        if (!transactionTypeTD) continue;
-
-        const type = transactionTypeTD.textContent.trim();
-        if (!validTXNTypes.includes(type)) continue;
-
-        if (selected.includes("Select All") || selected.includes(type))
-        {
-            row.classList.remove("hidden");
-        }
-        else
-        {
-            row.classList.add("hidden");
-        }
-    }
-
-});
-
-/*
-<tr id="hardwaretransaction_viewtransactiondetails_314378540"
-name="data[HardwareTransaction][viewTransactionDetails][314378540]"
-style="height:25px;" class="oddRow">
- */
-
 // Cloud Instance Reclaim
 // Cloud Instance Reload
 // Cloud Migrate
@@ -43,9 +5,46 @@ style="height:25px;" class="oddRow">
 // Create Cloud Disk Checksum
 // Export Image COS
 // Import Image COS
+// Juniper Reload
 // MS Cloud Provision
 // RH Reload
 // Reclaim
 // Storage Provision
 // UBU Reload
 // XenServer HV Reload
+
+const validTXNTypes = ["Cloud Instance Reclaim", "Cloud Instance Reload", "Cloud Migrate",
+    "Cloud Provision", "Create Cloud Disk Checksum", "Export Image COS", "Import Image COS",
+    "Juniper Reload", "MS Cloud Provision", "RH Reload", "Reclaim", "Storage Provision",
+    "UBU Reload", "XenServer HV Reload"];
+
+(() =>
+{
+    if (window.hasRun) return;
+    window.hasRun = true;
+
+    function filterTXNs(selected)
+    {
+        const tableRows = document.querySelectorAll("#layoutContentBody tbody tr");
+
+        for (let i = 0; i < tableRows.length; ++i) {
+            const row = tableRows[i];
+
+            const transactionTypeTD = row.children[5];
+            if (!transactionTypeTD) continue;
+
+            const type = transactionTypeTD.textContent.trim();
+            if (!validTXNTypes.includes(type)) continue;
+
+            if (selected.includes("Select All") || selected.includes(type))
+                row.classList.remove("hidden");
+            else
+                row.classList.add("hidden");
+        }
+    }
+
+    browser.runtime.onMessage.addListener(message =>
+    {
+        filterTXNs(message.selected);
+    });
+})();
